048b3a3971ad27f4c52267a48754d53a69ff63f9
陈芳 <chenfang@gl.com>
2022-03-23 15:24:06 +0800

    ...

--- main.js
+++ main.js
@@ -30,4 +30,24 @@ eventEmitter.on('data_received', function() {
   })
   // 触发connection事件
 eventEmitter.emit('connection')
-console.log('程序执行完毕')
+var EventEmitter = require('events')
+
+// --------------------------------------------------
+var fs = require("fs");
+
+// 创建一个可读流
+var readerStream = fs.createReadStream('input.txt');
+
+// 创建一个可写流
+var writerStream = fs.createWriteStream('output.txt');
+
+// 管道读写操作
+// 读取 input.txt 文件内容，并将内容写入到 output.txt 文件中
+readerStream.pipe(writerStream);
+
+console.log("程序执行完毕");
+
+var fs = require('fs')
+var readerStream = fs.createReadStream('input.txt')
+var writerStream = fs.createWriteStream('output.txt')
+readerStream.pipe(writerStream)
